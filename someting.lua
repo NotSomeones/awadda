e6c8ed8818bc201260daf6feb3c9fd5a03c0453b
NotSomeones <136521699+NotSomeones@users.noreply.github.com>
2025-06-21 17:51:28 -0400

    Update someting.lua

--- someting.lua
+++ someting.lua
@@ -1,11 +1,41 @@
-local farms = game.workspace.Farm
+local farms = game.Workspace.Farm
 local playername = game.Players.LocalPlayer.Name
+
 local function findfarm()
-    for i, v in pairs(farms:GetChildren()) do
+    for _, v in pairs(farms:GetChildren()) do
         if v.Important.Data.Owner.Value == playername then
             print("found it")
+            return v
         end
     end
 end
 
-findfarm()
+local myfarm = findfarm()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local seedshop = game.Players.LocalPlayer.PlayerGui.Seed_Shop
+
+local newlist = {}
+
+local function buyallstock()
+    for _, v in pairs(seedshop.Frame.ScrollingFrame:GetChildren()) do
+        local mainframe = v:FindFirstChild("Main_Frame")
+        if not mainframe then continue end
+
+        local stockTextObj = mainframe:FindFirstChild("Stock_Text")
+        if not stockTextObj then continue end
+
+        local seedname = v.Name
+        local stockCount = tonumber(stockTextObj.Text:match("%d+"))
+        if not stockCount or stockCount <= 0 then continue end
+
+        for _ = 1, stockCount do
+            ReplicatedStorage.GameEvents.BuySeedStock:FireServer(seedname)
+        end
+    end
+end
+
+while true do
+    wait(10)
+    buyallstock()
+end
